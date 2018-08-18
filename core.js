56e7692d919f635dd5d8bddeaf50cea461a578d2
jasonren5 <31326470+jasonren5@users.noreply.github.com>
2018-08-18 11:23:14 -0400

    cleaned up JS a little
    commented a bunch of things, added some TODO's, implemented a simple debug mode that disables the JS loaded alerts

--- core.js
+++ core.js
@@ -1,3 +1,5 @@
+//there has to be a better way than just initializing all of these variables at the start 
+
 var playerHp = 30;
 var playerMp = 0;
 var playerMun = 0;
@@ -7,15 +9,19 @@ var playerXp = 0;
 
 var playerMaxXp = playerMaxXp = Math.round(Math.pow(playerLevel, 1.1)) * 10 + 10;
 
-var playerAtkSpd = 100;
+var playerAtkSpd = 200;
 var enemyAtkSpd = 1000;
 
 var attack = 0;
 var attacking = false;
 
-var zone = 1;
+var zone = 1; //the zone (or difficulty). floored at 1
 
-alert('core.js loaded');
+var debug = 1; //1 activates debug mode
+
+if (debug == 1) {
+    alert('core.js loaded');
+}
 
 newEnemy();
 refresh();
@@ -24,9 +30,11 @@ document.getElementById('main-button').addEventListener('click', mainButtonClick
 document.getElementById('dec-diff').addEventListener('click', decDiff);
 document.getElementById('inc-diff').addEventListener('click', incDiff);
 
+//when the attack button is clicked
 function mainButtonClick() {
     if (attacking == false) {
-        attack = setInterval(playerAttack, playerAtkSpd);
+        enemyFight = setInterval(enemyAttack, enemyAtkSpd);
+        playerFight = setInterval(playerAttack, playerAtkSpd);
         attacking = true;
     }
     refresh();
@@ -35,24 +43,29 @@ function mainButtonClick() {
 function playerAttack() {
     enemy.hp = enemy.hp - playerStrength;
     if (enemy.hp < 1) {
+        clearInterval(enemyFight);
         newEnemy();
         rewardPlayer();
     }
     refresh();
 }
 
+//enemyattack function. TODO: currently the player cannot die
 function enemyAttack() {
-
+    playerHp = playerHp - enemy.str;
+    refresh();
 }
 
 function newEnemy() { //also called when an enemy is killed
-    //if (attacking == true) {
-    //clearInterval(attack);
+    //if (attacking == true) {  //TODO: remove these comments so new encounters do not start automatically
+    //clearInterval(playerFight);
     //}
     enemy = new Enemy(zone);
 }
 
-function rewardPlayer() { //is called when an enemy is killed
+//is called when an enemy is killed
+//TODO: revise money/xp formulas; XP needs to be tuned up at higher levels
+function rewardPlayer() {
     if (random(10, 4)) {
         playerMun += Math.round(enemy.maxHp * (Math.random() * 2) / 10);
     }
@@ -77,6 +90,9 @@ function refresh() { //refreshes numbers. TODO: make this more efficient?
     document.getElementById('diff').innerHTML = zone;
 }
 
+
+//called whenever the player earns XP
+//  TODO: maybe should calculate playerMaxXp somewhere else?
 function checkLevelUp() {
     playerMaxXp = Math.round(Math.pow(playerLevel, 1.1)) * 10 + 10;
     while (playerXp >= playerMaxXp) {
@@ -84,6 +100,8 @@ function checkLevelUp() {
     }
 }
 
+//called when the character's xp has surpassed the maxxp. Increases the level of the player,
+//  and also increases their power (so far only includes HP and Str)
 function levelUp() {
     playerLevel++;
     playerXp -= playerMaxXp;
@@ -91,6 +109,7 @@ function levelUp() {
     playerHp += 5;
 }
 
+//Used when the '-' button is clicked to decrease the difficulty of the enemy
 function decDiff() {
     if (zone > 1) {
         zone--;
@@ -98,6 +117,7 @@ function decDiff() {
     }
 }
 
+//Used when the '+' difficulty is clicked to increase the difficulty of the zone
 function incDiff() {
     zone++
     newEnemy();
--- enemy.js
+++ enemy.js
@@ -1,4 +1,6 @@
-alert('EnemyScript.js loaded');
+if (debug == 1) {
+    alert('EnemyScript.js loaded');
+}
 
 var baseMobNames = [
     "rat",
@@ -16,6 +18,7 @@ class Enemy {
     constructor(difficulty) {
         this.maxHp = Math.round(Math.pow(10 * difficulty, 1 + difficulty / 10));
         this.hp = this.maxHp;
+        this.str = 1;
         this.name = generateEnemyName();
     }
 
